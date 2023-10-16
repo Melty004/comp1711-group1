8d91b8b784257bbd6d1524730fb903651586d4c6
Hanxiao Deng <146663678+Melty004@users.noreply.github.com>
2023-10-16 09:55:51 +0000

    if_else

---
+++ week3/worksheet.c
@@ -0,0 +1,95 @@
+// #include <stdio.h>
+// // Positive, Negative, Zero
+
+// int main()
+// {
+//     int num;
+//     printf("Please enter a number: \n");
+//     scanf("%d", &num);
+
+//     if (num == 0){
+//         printf("The number of %d is zero\n", num);
+//     }
+//     else if (num < 0){
+//         printf("The number of %d is negative\n", num);
+//     }
+//     else{
+//         printf("The number of %d is positive\n", num);
+//     }
+
+//     return 0;
+// }
+
+// #include<stdio.h>
+// // Divsible by 4 and 5
+// int main()
+// {
+//     int num;
+//     printf("Please enter a number: \n");
+//     scanf("%d", &num);
+
+//     if (num % 4 == 0 && num % 5 ==0){
+//         printf("%d is divisible by both 4 and 5\n", num);
+//     }
+//     else{
+//         printf("false\n");
+//     }
+
+//     return 0;
+// }
+
+// #include<stdio.h>
+// // Grade Cheker with Distinction
+// int main(){
+//     int mark;
+//     printf("Please enter the mark: \n");
+//     scanf("%d", &mark);
+
+//     if (mark >= 70)
+//     {
+//         printf("Distinction\n");
+//     }
+//     else if (mark >=50 && mark < 70)
+//     {
+//         printf("Pass\n");
+//     }
+//     else
+//     {
+//         printf("Failed\n");
+//     }
+    
+//     return 0;
+// }
+
+// #include<stdio.h>
+// // Temperature Range Validator
+// int main(){
+//     int temp;
+//     printf("Enter the temperature with degrees Celsius: \n");
+//     scanf("%d", &temp);
+
+//     if (temp >= -10 && temp <=40)
+//     {
+//         printf("The temperature is %d Celsius.\n", temp);
+//     }
+//     else
+//     {
+//         printf("False\n");
+//     }
+
+//     return 0;
+// }
+
+#include<stdio.h>
+
+int main(){
+    int a;
+    a = 4;
+
+    switch (a)
+    {
+        case 0: printf("")
+    }
+
+    return 0;
+}
